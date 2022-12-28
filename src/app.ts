bc46baa148864209db5c026cc36e6da31ba605ea
Tanmay Vaij <tanmayvaij22@gmail.com>
2022-12-28 19:16:14 +0530

    added comments in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+// top level imports
 import express from "express"
 import { config } from "dotenv"
 import cors from "cors"
@@ -8,24 +9,30 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 
+// enabled all environment variables
 config()
 
 
+// server settings
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors())
 
 
+// importing all routers 
 import AuthRouter from "./routes/Auth"
 
 
+// using all routers
 app.use('/api/auth', AuthRouter)
 
 
+// starting the server
 const start = () => {
 
     try {
 
+        // connecting to database
         connectDB()
 
         app.listen(PORT, () => {
